e5d2f380d87f070b6f40f51a7c300090d9ee64d9
AnsarulHaq786 <mdansarul786940@gmail.com>
2024-07-13 11:23:24 +0545

    implement javascript over tic tac toe game

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,29 +1,54 @@
-
-//incomplete|errors
-
-let boxes=document.querySelectorAll(".boxes");
-let one=document.getElementById("1");
-let two=document.getElementById("2");
-let three=document.getElementById("3");
-let four=document.getElementById("4");
-let five=document.getElementById("5");
-let six=document.getElementById("6");
-let seven=document.getElementById("7");
-let eight=document.getElementById("8");
-let nine=document.getElementById("9");
-let turn='O';
-let btn=document.getElementsByClassName("btn");
-btn.addEventListener("hover",()=>{
-    btn.style.backgroundColor="red";
+let container=document.querySelector(".container");
+let btns=document.querySelectorAll(".btn");
+let resetBtn=document.querySelector("#reset");
+let msg=document.querySelector(".msg");
+let turnO=true;
+let winPattern=[
+    [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
+];
+btns.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        if(turnO){
+            btn.innerText='O';
+            turnO=false;
+        }
+        else{
+            btn.innerText='X';
+            turnO=true;
+        }
+        btn.setAttribute("disabled", true);
+        checkWinner();
+    })
 })
-btn.addEventListener("click",()=>{
-    console.log("button clicked!");
-    if(turn=='O'){
-        one.innerText('O');
-        turn='X';
+const checkWinner=()=>{
+    for(let pattern of winPattern){
+        let p1=btns[pattern[0]-1].innerText;
+        let p2=btns[pattern[1]-1].innerText;
+        let p3=btns[pattern[2]-1].innerText;
+        if(p1!="" && p2!="" && p3!=""){
+            if(p1==p2 && p2==p3){
+                winner(p1);
+                resetGame();
+            }
+        }
     }
-    else{
-       two.innerText('X');
-       turn='O'; 
+}
+const resetGame=()=>{
+    btns.forEach((btn)=>{
+        btn.innerText="";
+        btn.removeAttribute("disabled");
     }
+)}
+const winner=(p1)=>{
+    const msg = document.querySelector(".msg");
+    container.style.display='none';
+    msg.innerText = `${p1} is winner`;
+    msg.style.display='flex';
+    setTimeout(() => {
+        container.style.display='flex';
+        msg.style.display = 'none';
+    }, 1000);
+}
+resetBtn.addEventListener("click",()=>{
+    resetGame();
 })
