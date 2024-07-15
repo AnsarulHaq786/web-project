686c9f4aff6dd60903a8b26950ac7a8824676b09
AnsarulHaq786 <mdansarul786940@gmail.com>
2024-07-15 20:30:03 +0545

    add draw logic into the game

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -2,7 +2,7 @@ let container=document.querySelector(".container");
 let btns=document.querySelectorAll(".btn");
 let resetBtn=document.querySelector("#reset");
 let msg=document.querySelector(".msg");
-let turnO=true;
+let turnO=true, count=0;
 let winPattern=[
     [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
 ];
@@ -21,12 +21,13 @@ btns.forEach((btn)=>{
     })
 })
 const checkWinner=()=>{
+    count++;
     for(let pattern of winPattern){
         let p1=btns[pattern[0]-1].innerText;
         let p2=btns[pattern[1]-1].innerText;
         let p3=btns[pattern[2]-1].innerText;
-        if(p1!="" && p2!="" && p3!=""){
-            if(p1==p2 && p2==p3){
+        if((p1!="" && p2!="" && p3!="") || count==9){
+            if((p1==p2 && p2==p3) || count==9){
                 winner(p1);
                 resetGame();
             }
@@ -34,6 +35,7 @@ const checkWinner=()=>{
     }
 }
 const resetGame=()=>{
+    count=0;
     btns.forEach((btn)=>{
         btn.innerText="";
         btn.removeAttribute("disabled");
@@ -42,7 +44,12 @@ const resetGame=()=>{
 const winner=(p1)=>{
     const msg = document.querySelector(".msg");
     container.style.display='none';
-    msg.innerText = `${p1} is winner`;
+    if(count==9){
+        msg.innerText = `Draw : you both played well!`
+    }
+    else{
+        msg.innerText = `${p1} is winner`;
+    }
     msg.style.display='flex';
     setTimeout(() => {
         container.style.display='flex';
