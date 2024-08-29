620b01408995eb49d17512c1837315fbb40b624c
AnsarulHaq786 <mdansarul786940@gmail.com>
2024-08-29 20:42:06 +0545

    use event object to implement checked, unchecked state

--- todo-list/script.js
+++ todo-list/script.js
@@ -10,6 +10,18 @@ function addToList(){
         let li = document.createElement("li");
         li.innerHTML = inputText.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputText.value = '';
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
